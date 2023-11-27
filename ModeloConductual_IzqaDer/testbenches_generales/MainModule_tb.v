3bbf1af3aaec4d6a3410d2c035efcbbba0554f51
Cesar Andres Fonseca Villegas <perryypipo2@gmail.com>
2023-11-26 23:38:52 -0600

    test bench con el for en la celda general

--- ModeloConductual_IzqaDer/testbenches_generales/MainModule_tb.v
+++ ModeloConductual_IzqaDer/testbenches_generales/MainModule_tb.v
@@ -1,49 +1,41 @@
 // MainModule_tb.v
 `timescale 1 ns/10 ps // time-unit = 1 ns, precision = 10 ps
 module MainModule_tb();
-  reg x, y, A, B;
+  reg z, y;
   wire f;
- // duration for each bit = 20 * timescale = 20 * 1 ns = 20ns
- localparam period = 20;
- system UUT (.A(A), .B(B), .x(x), .y(y), .f(f));
+  reg [2:0] miVectorA;
+  reg [2:0] miVectorB;
 
- initial // initial block executes only once
- begin
- $dumpfile ("MainModule_tb.vcd");
- $dumpvars(1, MainModule_tb);
+  // duration for each bit = 20 * timescale = 20 * 1 ns = 20ns
+  localparam period = 20;
+  system UUT (.A(miVectorA), .B(miVectorB), .z(z), .y(y), .f(f));
 
- // values for a, b, x and y
- A = 0; B = 0; x = 0; y = 0;
- #period; // wait for period
- A = 0; B = 0; x = 0; y = 1;
- #period;
- A = 0; B = 0; x = 1; y = 0;
- #period;
- A = 0; B = 0; x = 1; y = 1;
- #period;
- A = 0; B = 1; x = 0; y = 0;
- #period;
- A = 0; B = 1; x = 0; y = 1;
- #period;
- A = 0; B = 1; x = 1; y = 0;
- #period;
- A = 0; B = 1; x = 1; y = 1;
- #period;
- A = 1; B = 0; x = 0; y = 0;
- #period; 
- A = 1; B = 0; x = 0; y = 1;
- #period;
- A = 1; B = 0; x = 1; y = 0;
- #period;
- A = 1; B = 0; x = 1; y = 1;
- #period;
- A = 1; B = 1; x = 0; y = 0;
- #period;
- A = 1; B = 1; x = 0; y = 1;
- #period;
- A = 1; B = 1; x = 1; y = 0;
- #period;
- A = 1; B = 1; x = 1; y = 1;
- #period;
- end
+  initial // initial block executes only once
+  begin
+    $dumpfile("MainModule_tb.vcd");
+    $dumpvars(1, MainModule_tb);
+
+    // valores para a, b, x e y
+    miVectorA = 3'b111;
+    miVectorB = 3'b001;
+
+    // bucle para comparar los bits de los vectores
+    //integer n;
+    
+    for (integer n = 0; n < 3; n = n + 1) begin
+      // Compara los términos correspondientes de los vectores
+      if (miVectorA[n] == miVectorB[n]) begin
+        y = 0;
+        z = 1;
+      end else if (miVectorA[n] > miVectorB[n]) begin
+        y = 1;
+        z = 0;
+      end else begin
+        y = 1;
+        z = 1;
+      end
+
+      $display("Para n = %0d, y = %0d, z = %0d, f = %0d", n, y, z, f);
+    end
+  end
 endmodule
